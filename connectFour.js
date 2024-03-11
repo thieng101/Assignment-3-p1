a7ab5b568f61b5e90954dd4df50f1dfed92a2039
Hong Thy Nguyen <hongthy3101@gmail.com>
2024-03-11 16:53:59 -0400

    completed code with all function works

--- connectFour.js
+++ connectFour.js
@@ -1,8 +1,3 @@
-// Initial references
-// Create global variables for the different elements in the document
-// The querySelector() method returns the first element that matches a CSS selector
-// The getElementById() method returns the element that matches the ID
-
 // Declare constant container set equal to object document, method querySelector, passing as an argument class ".container"
 const container = document.querySelector(".container");
 // Declare constant playerTurn set equal to object document, method getElementById, passing as an argument id "playerTurn"
@@ -14,59 +9,155 @@ var initialMatrix = new Array(6).fill(0).map(() => new Array(7).fill(0));
 // Declare variable currentPlayer to store the current player
 var currentPlayer = 1;
 
-// Write function gameOverCheck
 function gameOverCheck() {
-  console.log("gameOverCheck");
+  let count = 0;
+  for (const innerArray of initialMatrix) {
+    if (innerArray.every((val) => val !== 0)) {
+      count++;
+    } else {
+      return false;
+    }
+  }
+
+  if (count === 6) {
+    message.innerText = "Game Over";
+    return false;
+  }
+}
+
+function winCheck(row, column) {
+  if (
+    checkVertical(row, column) ||
+    checkHorizontal(row, column) ||
+    checkPositiveDiagonal(row, column) ||
+    checkNegativeDiagonal(row, column)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function checkVertical(row, column) {
+  for (let i = row; i < 6; i++) {
+    for (let j = column; j < 7; j++) {
+      if (initialMatrix[i][j] === currentPlayer) {
+        if (
+          initialMatrix[i + 1] &&
+          initialMatrix[i + 1][j] === currentPlayer &&
+          initialMatrix[i + 2] &&
+          initialMatrix[i + 2][j] === currentPlayer &&
+          initialMatrix[i + 3] &&
+          initialMatrix[i + 3][j] === currentPlayer
+        ) {
+          return true;
+        }
+      }
+    }
+  }
   return false;
 }
 
-// Write function winCheck
-function winCheck() {
-  console.log("winCheck");
+function checkHorizontal(row, column) {
+  for (let i = row; i < 6; i++) {
+    for (let j = column; j < 7; j++) {
+      // console.log(column);
+      if (initialMatrix[i][j] === currentPlayer) {
+        if (
+          initialMatrix[i] &&
+          initialMatrix[i][j - 1] &&
+          initialMatrix[i][j - 1] === currentPlayer &&
+          initialMatrix[i] &&
+          initialMatrix[i][j - 2] &&
+          initialMatrix[i][j - 2] === currentPlayer &&
+          initialMatrix[i] &&
+          initialMatrix[i][j - 3] &&
+          initialMatrix[i][j - 3] === currentPlayer
+        ) {
+          return true;
+        }
+      }
+    }
+  }
   return false;
 }
 
+function checkPositiveDiagonal(row, column) {
+  for (let i = row; i < 6; i++) {
+    for (let j = column; j < 7; j++) {
+      if (initialMatrix[i][j] === currentPlayer) {
+        if (
+          initialMatrix[i + 1] &&
+          initialMatrix[i + 1][j - 1] === currentPlayer &&
+          initialMatrix[i + 2] &&
+          initialMatrix[i + 2][j - 2] === currentPlayer &&
+          initialMatrix[i + 3] &&
+          initialMatrix[i + 3][j - 3] === currentPlayer
+        ) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+function checkNegativeDiagonal(row, column) {
+  for (let i = row; i < 6; i++) {
+    for (let j = column; j < 7; j++) {
+      if (initialMatrix[i][j] === currentPlayer) {
+        if (
+          initialMatrix[i + 1] &&
+          initialMatrix[i + 1][j + 1] === currentPlayer &&
+          initialMatrix[i + 2] &&
+          initialMatrix[i + 2][j + 2] === currentPlayer &&
+          initialMatrix[i + 3] &&
+          initialMatrix[i + 3][j + 3] === currentPlayer
+        ) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
 // Write function setPiece
 function setPiece(startCount, colValue) {
-  console.log("setPiece");
-
   // Declare variable rows initialized to object document, method querySelectorAll, passing argument class ".grid-row"
   var rows = document.querySelectorAll(".grid-row");
   // If the element in array initialMatrix at indexes parameters startCount and colValue is NOT equal to 0
-  if (initialMatrix[startCount][colValue] !== 0) {
-    // Decrement parameter startCount by 1
-    startCount--;
-    // Call function setPiece, passing as arguments parameters startCount and colValue
-    setPiece(startCount, colValue);
-  }
-  // Else
-  else {
-    // Declare variable currentRow initialized to array rows, index startCount, method querySelectorAll, passing as an argument class ".grid-box"
-    // Modify currentRow, index colValue, object classlist, method add, passing as arguments "filled" and `player${currentPlayer}`
-    var currentRow = rows[startCount].querySelectorAll(".grid-box");
-    currentRow[colValue].classList.add("filled", `player${currentPlayer}`);
-
-    // Update array initialMatrix, indexes startCount and colValue, set equal to currentPlayer
-    initialMatrix[startCount][colValue] = currentPlayer;
-    // If function call winCheck, passing as arguments parameters startCount and colValue is true
-    if (winCheck(startCount, colValue)) {
-      // Call function gameOver, passing as an argument currentPlayer
-      gameOver(currentPlayer);
+  try {
+    if (initialMatrix[startCount][colValue] !== 0) {
+      // Decrement parameter startCount by 1
+      startCount--;
+      // Call function setPiece, passing as arguments parameters startCount and colValue
+      setPiece(startCount, colValue);
     }
-    // Set object message's innerHTML equal to `Player<span> ${currentPlayer}</span> wins`
-    message.innerHTML = `Player<span> ${currentPlayer}</span> wins`;
-
-    // Return false
-    return false;
+    // Else
+    else {
+      // Declare variable currentRow initialized to array rows, index startCount, method querySelectorAll, passing as an argument class ".grid-box"
+      var currentRow = rows[startCount].querySelectorAll(".grid-box");
+      // Modify currentRow, index colValue, object classlist, method add, passing as arguments "filled" and `player${currentPlayer}`
+      currentRow[colValue].classList.add("filled", `player${currentPlayer}`);
+      // Update array initialMatrix, indexes startCount and colValue, set equal to currentPlayer
+      initialMatrix[startCount][colValue] = currentPlayer;
+      // If function call winCheck, passing as arguments parameters startCount and colValue is true
+      if (winCheck(startCount, colValue)) {
+        // Set object message's innerHTML equal to `Player<span> ${currentPlayer}</span> wins`
+        message.innerHTML = `Player<span> ${currentPlayer}</span> wins`;
+        // Return false
+        return false;
+      }
+      // Call function gameOverCheck
+      gameOverCheck();
+    }
+  } catch (e) {
+    alert("Column full, select again");
   }
-
-  // Call function gameOverCheck
-  gameOverCheck();
 }
 
 // Write function fillBox
 function fillBox(e) {
-  console.log("fillBox");
   // Declare variable colValue set equal to function parseInt() of parameter e, object target, function getAttribute, passing as argument "data-value"
   var colValue = parseInt(e.target.getAttribute("data-value"));
 
@@ -82,12 +173,9 @@ function fillBox(e) {
 
 // Write function createBoard
 function createBoard() {
-  console.log("createBoard");
-
   // Iterate through the 2d array initialMatrix
   // Write an outer for in loop to iterate through the rows, loop control variable innerArray
   for (let innerArray = 0; innerArray < 6; innerArray++) {
-    // console.log("we are in first loop");
     // Declare variable outerDiv set equal to object document, method createElement, passing "div" as an argument
     var outerDiv = document.createElement("div");
     // Modify outerDiv, classList, to add class "grid-row"
@@ -97,14 +185,9 @@ function createBoard() {
     outerDiv.setAttribute("data-value", innerArray);
 
     // Write an inner for in loop to iterate through the columns, loop control variable j
-    // console.log("we are in betweeen loop");
     for (let j = 0; j < 7; j++) {
-      console.log("we are in second loop");
-
       // Set each element in array initialMatrix to the value of 0
-      console.log("index" + innerArray);
       initialMatrix[innerArray][j] = 0;
-      console.log(initialMatrix[innerArray][j]);
 
       // Declare variable innerDiv set equal to object document, method createElement, passing "div" as an argument
       var innerDiv = document.createElement("div");
@@ -130,8 +213,6 @@ function createBoard() {
 
 // Write function startGame
 function startGame() {
-  console.log("startGame");
-
   // Set currentPlayer to 1, player 1 always goes first
   currentPlayer = 1;
 
@@ -146,4 +227,4 @@ function startGame() {
 }
 
 // For the window.onload event, call function startGame
-window.onload = startGame;
+window.onload = startGame();
